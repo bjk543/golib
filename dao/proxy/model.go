f2ec8afb5f84c33ce324f448f6f9b31ad3c6e0aa
manny shen <mannyshen@starlordtech.com>
2020-12-25 10:01:10 +0800

    q

--- dao/proxy/model.go
+++ dao/proxy/model.go
@@ -3,9 +3,9 @@ package proxy
 import "time"
 
 type Proxy struct {
-	Addr      string    `gorm:"primary_key;type:varchar(10);"`
-	Success   uint64    `gorm:"type:decimal(4);default:0"`
-	Fail      uint64    `gorm:"type:decimal(4);default:0"`
+	Addr      string    `gorm:"primary_key;type:varchar(24);"`
+	Success   uint64    `gorm:"type:decimal(5);default:0"`
+	Fail      uint64    `gorm:"type:decimal(5);default:0"`
 	Active    bool      `gorm:"default:true"`
 	CreatedAt time.Time `gorm:"type:timestamp;not null;default:now()"`
 }
