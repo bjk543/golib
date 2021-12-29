771466f51737026c77f0ca1e76857d134f208043
shen <shen@gmail.com>
2021-12-29 17:19:32 +0800

    logger

--- dao/conn.go
+++ dao/conn.go
@@ -2,11 +2,13 @@ package dao
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func Close(db *gorm.DB) {
@@ -19,9 +21,17 @@ func Close(db *gorm.DB) {
 func CreateConn(user, pass, host, port, dbName string) *gorm.DB {
 	var db *gorm.DB
 	var err error
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Disable color
+		},
+	)
 	for i := 0; i < 5; i++ {
 		dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable TimeZone=Asia/Taipei", host, port, user, dbName, pass)
-		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{newLogger: newLogger})
 		// db, err = gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", host, port, user, dbName, pass))
 		if err != nil {
 			log.WithFields(log.Fields{
@@ -57,7 +67,6 @@ func CreateConn(user, pass, host, port, dbName string) *gorm.DB {
 		ddb.SetConnMaxLifetime(60 * time.Second) //这个时间和lb的idle超时短就行了
 		ddb.SetMaxIdleConns(0)                   //不要使用连接池
 	}
-	db.LogMode(true)
 
 	return db
 }
